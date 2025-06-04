1a26c42abfc85256d9fd7695128515e716c5d8e8
chamath1991 <chamath.erplanka@gmail.com>
2025-06-04 21:44:12 +0530

    home page only the ui

---
+++ src/components/HomePage/ProfileDetails.vue
@@ -0,0 +1,56 @@
+<template>
+    <Card>
+        <template #header>
+            
+        </template>
+        <template #content>
+                <div class="d-flex justify-content-between" >
+                <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" style="width:70px;height:70px;" @click="toggle"/>
+                <i class="pi pi-info-circle"></i>
+                </div>
+                <h6 class="p-0 m-0 mt-2 mb-2">Chamath Rathnayake</h6>
+                <p class="p-0 m-0 fw-semibold text-secondary" style="font-size:13px;">Itellegence Pvt Ltd</p>
+                <p class="p-0 fw-semibold" style="font-size:13px;">Colombo</p>
+
+                <Divider >
+                <span class="text-secondary" style="font-size:12px;">My Categories</span>
+                    <i class="pi pi-ellipsis-v border rounded-circle p-1 bg-dark text-white mx-1" style="font-size:8px;" @click="showMenu"></i>
+                    <Menu ref="menu" :model="items" :popup="true" />
+                </Divider>
+                    <div class="d-flex justify-content-end align-content-center">
+                        
+                    </div>
+                    <p class="w-100 d-flex justify-content-between align-content-center">Action <i class="pi pi-trash" /></p>  
+                    <p class="w-100 d-flex justify-content-between align-content-center">Refree <i class="pi pi-trash" /></p>
+                    <p class="w-100 d-flex justify-content-between align-content-center">Action <i class="pi pi-trash" /></p>
+
+        </template>
+    </Card>
+</template>
+<script setup lang="ts">
+
+import Menu from 'primevue/menu';
+import Divider from 'primevue/divider';
+import Avatar from 'primevue/avatar';
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+import { ref } from 'vue';
+const categories = ref([
+    { name: 'Category 1', code: 'C1' },
+    { name: 'Category 2', code: 'C2' },
+    { name: 'Category 3', code: 'C3' }
+]);
+
+const items = ref([
+    { label: 'Add Category', icon: 'pi pi-fw pi-plus', command: () => { } },
+    { label: 'Attached Categories', icon: 'pi pi-fw pi-file', command: () => { } },
+    { label: 'Sort Categories', icon: 'pi pi-fw pi-sort', command: () => { } }
+]);
+const menu= ref(null);
+const selectedCity= ref(null);
+const showMenu = (event: Event) => {
+    menu.value.toggle(event);
+}
+
+
+</script>
---
+++ src/components/HomePage/QuestionGroups.vue
@@ -0,0 +1,61 @@
+<template>
+     <Card class="mb-3" id="group1">
+        <template #title>
+            <div class="d-flex justify-content-between align-items-center justify-content-center border-bottom pb-2"> 
+                <div class="d-flex flex-column">
+                    <span class="fs-5"> Question Group 01</span>
+                    <span style="font-size:12px;" class="text-secondary fw-semibold"> Category : Category 01 
+                    <span class="mx-2">
+                            <i class="pi pi-star-fill" style="font-size:12px;color:#a03829;"/>
+                            <i class="pi pi-star-fill" style="font-size:12px;color:#a03829;"/>
+                            <i class="pi pi-star-fill" style="font-size:12px;color:#a03829;"/>
+                    </span>
+                    </span>
+                </div>
+                <i class="pi pi-ellipsis-v cursor-pointer sorting-icon" style="font-size:14px;" @click="toggle($event)"></i>
+                <Menu ref="menuShow" :model="items" :popup="true">
+                    <template #start>
+                        <div class="d-flex justify-content-end align-items-center">
+                            
+                            
+                        </div>
+                    </template>
+                    <template #item="{ item, props }" v-bind="props.action">
+                        <div class="p-2 d-flex align-items-center justify-content-between">
+                            <span>
+                                <span :class="item.icon" />
+                                <span class="text-dark m-2">{{ item.label }}</span>
+                            </span>
+                        </div>
+                        
+                    </template>
+                </Menu>
+                
+            </div>
+                
+        </template>
+        <template #content>
+            <p class="m-0">
+                orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </template>
+    </Card>
+</template>
+<script setup lang="ts">
+import Card from 'primevue/card';
+import { ref } from 'vue';
+import Menu from 'primevue/menu';
+const menuShow = ref<Menu | null>(null);
+const items = ref([
+    { label: 'Skip', command: () => { console.log('Sorting by Recent'); },icon:'pi pi-forward'},
+    { label: 'Start Test', command: () => { console.log('Sorting by Recent'); }, icon:'pi pi-play'},
+    { label: 'Open Test', command: () => { console.log('Sorting by Recent'); }, icon:'pi pi-folder-open'}
+]);
+
+const toggle = (event: Event) => {
+    // Logic to show or hide question groups
+    console.log('Toggle question group visibility');
+    menuShow.value?.toggle(event);
+};
+
+</script>
